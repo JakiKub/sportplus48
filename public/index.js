47d75992c3c107acd1b4f1a683211bead7a9d8a8
JakiKub <jakubtupalski@gmail.com>
2025-08-23 18:21:33 +0200

    ui update and css clean-up

--- public/index.js
+++ public/index.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const userData = await response.json();
       localStorage.setItem("user", JSON.stringify(userData));
 
-      window.location.href = "/home.html";
+      window.location.href = "/dashboard.html";
     } catch (err) {
       alert("blad polaczenia");
       console.error(err);
@@ -71,10 +71,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("nationality").textContent = user.nationality || "none";
     document.getElementById("logInButton").style.display = "none";
     document.getElementById("registerButton").style.display = "none";
-    document.getElementById("logoHome").style.display = "none";
-    document.getElementById("NavbarHome").style.display = "none";
-    document.getElementById("NavbarHomeLoggedIn").style.display = "flex";
+    document.getElementById("logoDashboard").style.display = "none";
+    document.getElementById("navbarDashboardLoggedOut").style.display = "none";
+    document.getElementById("navbarDashboardLoggedIn").style.display = "flex";
     document.getElementById("profileLogOut").style.display = "flex";
+    document.getElementById("navbarDashboardLoggedInTop").style.display = "flex";
+    document.getElementById("usernameDashboardTop").textContent = user.username;
+    document.getElementById("emailDashboardTop").textContent = user.email;
     profileContainer.style.display = "block";
     document.body.classList.add("logged-in");
   }
