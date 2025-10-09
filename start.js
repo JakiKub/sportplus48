d0887987e013cd0034c056b45035b7ef097a8f16
JakiKub <jakubtupalski@gmail.com>
2025-10-09 21:43:51 +0200

    hell nah

--- start.js
+++ start.js
@@ -12,9 +12,7 @@ const session = require('express-session');
 const mongoStore = require('connect-mongo');
 const connect = mongoose.connect(process.env.MONGO_URI);
 const upload = multer({ storage: multer.memoryStorage() });
-const fetch = require("node-fetch");
 const cloudinary = require("cloudinary").v2;
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 
 const app = express();
