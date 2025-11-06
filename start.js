7598593c90f0c5430d7c34238d4a2bcc3bde688d
JakiKub <jakubtupalski@gmail.com>
2025-11-06 22:28:56 +0100

    guess what, mam surprise dla you. mail

--- start.js
+++ start.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/content', express.static(path.join(__dirname, 'content')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 app.use(session({
