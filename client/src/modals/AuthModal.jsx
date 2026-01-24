d8c74ad001e22b24b0fe7b53b48d07920a5cf809
JakiKub <jakubtupalski@gmail.com>
2026-01-24 21:59:54 +0100

    ninth try

--- client/src/modals/AuthModal.jsx
+++ client/src/modals/AuthModal.jsx
@@ -14,7 +14,7 @@ const AuthModal = ({ mode, onClose, setUser }) => {
 
   const handleSubmit = async () => {
     if (isLogin) {
-      const res = await fetch("/login", {
+      const res = await fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
@@ -34,7 +34,7 @@ const AuthModal = ({ mode, onClose, setUser }) => {
     } else {
       if (!accepted) return alert("zaakceptuj regulamin");
 
-      const res = await fetch("/register", {
+      const res = await fetch("/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password, username, nationality: nation })
